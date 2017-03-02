c6996b434d25668cd81a571067d3586057853f84
chen1720 <chen1720@purdue.edu>
2017-03-02 14:34:15 -0600

    small changes

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -60,7 +60,7 @@ def evaluate(X_data, y_data):
     sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
         batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, label: batch_y})
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
 
